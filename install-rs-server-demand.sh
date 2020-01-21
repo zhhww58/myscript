f249dd95580fa45e4d67c2fe08d0b9abb399165f
zhhww58 <56435151+zhhww58@users.noreply.github.com>
2020-01-21 21:17:04 +0800

    Create install-rs-server-demand.sh

---
+++ install-rs-server-demand.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+systemctl stop rs-server
+systemctl disable rs-server
+
+echo |adduser rs --disabled-password --disabled-login
+su rs -c  'mkdir /home/rs/rs'
+cd /home/rs/rs
+rm -rf /home/rs/rs/russellcoind-linux-x64
+rm -rf /home/rs/rs/rs-cli
+rm -rf /etc/init.d/rs-server
+rm -rf russellcoind-linux-x64
+rm -rf rs-cli
+rm -rf russellcoin-64-245d
+rm -rf russellcoin-64-245-cli
+su rs -c  'wget https://github.com/zhhww58/myscript/raw/master/russellcoind-linux-x64'
+su rs -c  'wget https://github.com/zhhww58/myscript/raw/master/rs-cli'
+su rs -c  'chmod 755 rs-cli'
+su rs -c  'chmod 755 russellcoind-linux-x64'
+su rs -c  'mkdir /home/rs/.russellcoin2/'
+su rs -c  'rm -rf /home/rs/.russellcoin2/russellcoin.conf'
+su rs -c  'touch /home/rs/.russellcoin2/russellcoin.conf'
+
+echo server=1 > /home/rs/.russellcoin2/russellcoin.conf
+echo daemon=1 >> /home/rs/.russellcoin2/russellcoin.conf
+echo rpcuser=dashrpc >> /home/rs/.russellcoin2/russellcoin.conf
+echo rpcpassword=77tXVivCqw133ewfYwgWvziTU3SvsK7E5u9zCUZxauFu >> /home/rs/.russellcoin2/russellcoin.conf
+echo >>/home/rs/.russellcoin2/russellcoin.conf
+echo ##masternode conf##>>/home/rs/.russellcoin2/russellcoin.conf
+echo >>/home/rs/.russellcoin2/russellcoin.conf
+echo #masternode=1 >>/home/rs/.russellcoin2/russellcoin.conf
+echo #masternodeprivkey= >>/home/rs/.russellcoin2/russellcoin.conf
+echo #masternodeaddr= >>/home/rs/.russellcoin2/russellcoin.conf
+
+
+chmod 755 /home/rs/.russellcoin2/russellcoin.conf
+chown -R rs:rs /home/rs/.russellcoin2/russellcoin.conf
+rm -rf /usr/bin/start-rs
+rm -rf /usr/bin/rs-cli
+echo su -c '/home/rs/rs/russellcoind-linux-x64 $1 $2 $3 $4 $5 $6 $7' >/home/rs/rs/start-rs.sh
+echo su -c '/home/rs/rs/rs-cli $1 $2 $3 $4 $5 $6 $7' >/home/rs/rs/rs-cli.sh
+
+
+ln -s /home/rs/rs/russellcoind-linux-x64 /usr/bin/start-rs
+ln -s /home/rs/rs/rs-cli /usr/bin/rs-cli
+
+cd /etc/init.d/
+rm -rf rs-server
+
